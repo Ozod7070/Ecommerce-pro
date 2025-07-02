fedfa423c9c796ed6f95f6e15274d4657c0beddd
Umarbekdeveloper <umarbekbahromov07@gmail.com>
2025-07-03 00:39:32 +0500

    add

--- src/main/java/uz/pdp/service/CartService.java
+++ src/main/java/uz/pdp/service/CartService.java
@@ -1,4 +1,42 @@
 package uz.pdp.service;
 
-public class CartService {
+import uz.pdp.base.BaseService;
+import uz.pdp.modul.Cart;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class CartService implements BaseService<Cart> {
+    private static final String FILE_NAME = "carts.json";
+    private List<Cart> carts;
+
+    public CartService() {
+        try {
+            carts = readCarts();
+        } catch (IOException e) {
+            carts = new ArrayList<>();
+        }
+    }
+
+    @Override
+    public void add(Cart cart) throws Exception {
+
+    }
+
+    @Override
+    public void update(Cart cart, UUID id) throws Exception {
+
+    }
+
+    @Override
+    public boolean remove(Cart cart) throws Exception {
+        return false;
+    }
+
+    @Override
+    public Cart get(UUID id) throws Exception {
+        return null;
+    }
 }
