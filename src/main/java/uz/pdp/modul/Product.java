cec198b75af25718ada80038d0b4945290afaa30
Shoxruzbek7707 <shoxruzqoshoqboyev778@gmail.com>
2025-07-02 23:51:26 +0500

    product

--- src/main/java/uz/pdp/modul/Product.java
+++ src/main/java/uz/pdp/modul/Product.java
@@ -1,4 +1,14 @@
 package uz.pdp.modul;
 
+import jdk.jfr.DataAmount;
+
+import java.util.UUID;
+
 public class Product {
+    private String name;
+    private double price;
+    private int quantity;
+    private String categoryId;
+    private UUID catergoryId;
+    private UUID sellerId;
 }
