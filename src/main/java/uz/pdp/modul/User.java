82e1a3ba13cb36a199d757dc14129e8e39f1c5c1
Ozod7070 <ozodabdiholiqov38@gmail.com>
2025-07-02 23:49:14 +0500

    Add

--- src/main/java/uz/pdp/modul/User.java
+++ src/main/java/uz/pdp/modul/User.java
@@ -1,4 +1,18 @@
 package uz.pdp.modul;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import uz.pdp.enums.UserRole;
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
 public class User {
+    private String fullName;
+    private String userName;
+    private String password;
+    private UserRole role;
+
+
 }
