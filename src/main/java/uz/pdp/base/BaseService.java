3f663dd62afb9ff4536b429db2c3cabe45a8a15d
Umarbekdeveloper <umarbekbahromov07@gmail.com>
2025-07-03 09:09:09 +0500

    add

--- src/main/java/uz/pdp/base/BaseService.java
+++ src/main/java/uz/pdp/base/BaseService.java
@@ -1,8 +1,5 @@
 package uz.pdp.base;
 
-import uz.pdp.exceptions.InvalidUserNameException;
-import uz.pdp.modul.User;
-
 import java.util.UUID;
 
 public interface BaseService<T>{
@@ -10,12 +7,9 @@ public interface BaseService<T>{
 
     void add(T t) throws Exception;
 
+    boolean update(T t, UUID id) throws Exception;
 
-    void update(T t, UUID id) throws Exception;
-
-
-    boolean remove(UUID id) throws Exception;
-
+    boolean remove(T t) throws Exception;
 
     T get(UUID id) throws Exception;
 }
